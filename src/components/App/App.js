25e2038efefc7755533ec625f0dcfa1dc35aaf3a
RuslanBoiko <boyko.ruslan78@gmail.com>
2024-06-11 22:08:55 -0400

    Hook useCallback

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,15 @@
-import {useState} from 'react';
+import {Component, useCallback, useEffect, useState} from 'react';
 import {Container} from 'react-bootstrap';
 import './App.css';
+
+
+/* const getSomeImages = () => {
+    console.log('Fetching');
+    return [
+        "https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg",
+        "https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg",
+    ];
+}; */
 
 // class Slider extends Component {
 
@@ -28,7 +37,16 @@ import './App.css';
 //         return (
 //             <Container>
 //                 <div className="slider w-50 m-auto">
-//                     <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
+//                     {/* <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" /> */}
+
+//                     {
+//                         getSomeImages().map((url, i) => {
+//                             return (
+//                                 <img key={i} className="d-block w-100" src={url} alt="slide" />
+//                             )
+//                         })
+//                     }
+
 //                     <div className="text-center mt-5">Active slide {this.state.slide} <br/> {this.state.autoplay ? 'auto' : null}</div>
 //                     <div className="buttons mt-3">
 //                         <button 
@@ -53,6 +71,14 @@ const Slider = (props) => {
     const [slide, setSlide] = useState(0);
     const [autoplay, setAutoplay] = useState(false);
 
+    const getSomeImages = useCallback(() => {
+        console.log('Fetching');
+        return [
+            "https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg",
+            "https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg",
+        ];
+    }, [slide]);
+
     const changeSlide = (i) => {
         setSlide(slide => slide + i);
     }
@@ -64,7 +90,18 @@ const Slider = (props) => {
     return (
         <Container>
             <div className="slider w-50 m-auto">
-                <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
+                {/* <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" /> */}
+
+                {/* {
+                    getSomeImages().map((url, i) => {
+                        return (
+                            <img key={i} className="d-block w-100" src={url} alt="slide" />
+                        )
+                    })
+                } */}
+
+                <Slide getSomeImages={getSomeImages} />
+                
                 <div className="text-center mt-5">Active slide {slide} <br/> {autoplay ? 'auto' : null}</div>
                 <div className="buttons mt-3">
                     <button 
@@ -82,6 +119,23 @@ const Slider = (props) => {
     )
 }
 
+const Slide = ({getSomeImages}) => {
+    const [images, setImages] = useState([]);
+
+    useEffect(() => {
+        setImages(getSomeImages());
+    }, [getSomeImages]);
+
+    return (
+        <>
+            {
+                images.map((url, i) => (
+                    <img key={i} className="d-block w-100" src={url} alt="slide" />
+                ))
+            }
+        </>
+    );
+}
 
 function App() {
     return (
